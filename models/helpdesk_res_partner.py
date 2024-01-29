49cef1665b46c3f137f8dc1f700e8fe544dc53f6
root <root@CIMT-MATE>
2024-01-29 10:11:19 +0700

    Remove partner filter from helpdesk module

--- models/helpdesk_res_partner.py
+++ models/helpdesk_res_partner.py
@@ -27,12 +27,6 @@ class HelpdeskResPartner(models.Model):
         store=True,  # Storing the value is optional depending on your needs
     )
 
-
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        args += [('is_client', '=', True)]
-        return super(HelpdeskResPartner, self).search(args, offset, limit, order, count=count)
-
     @api.depends('sale_order_ids.contract_end_date')  # Assuming sale_order_ids is the reverse one2many relation
     def _compute_max_contract_end_date(self):  # Make sure this matches the name in the field definition
         for partner in self:
